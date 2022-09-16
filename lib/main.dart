964668974c0dbfff6f8041b315b2d5cab631a248
Oscar Mejia <oscarmarmejia@gmail.com>
2022-09-16 14:35:28 -0400

    favorite update v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
